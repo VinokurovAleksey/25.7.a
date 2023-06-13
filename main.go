d831736d7576fd452d525562a2d1c50006e4a545
Vinokurov Aleksey <jupiter4522@yandex.ru>
2023-06-13 21:54:09 +0900

    хотфикс

--- main.go
+++ main.go
@@ -7,10 +7,12 @@ import (
 
 func main() {
 	var n string
+	fmt.Println("Добро пожаловать!")
 	fmt.Print("Введите данные: ")
 	_, err := fmt.Scan(&n)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("Вы ввели следующие данные: ", n)
+	fmt.Println("Всего Вам доброго!")
 }
